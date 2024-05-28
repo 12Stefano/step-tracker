6dd58c098cc093394aec2ef5108d2e59d32ca73c
Stefano Pozzi <stefanopozzi29@gmail.com>
2024-05-28 22:43:38 +0200

    Initial Commit

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Stefano Pozzi on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
