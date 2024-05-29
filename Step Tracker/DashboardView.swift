38c4dec7ce0ca253695bd2fa45df85e300381257
Stefano Pozzi <stefanopozzi29@gmail.com>
2024-05-29 15:51:39 +0200

    Renamed ContentView in DashboardView and added HealtDataListView

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  DashboardView.swift
 //  Step Tracker
 //
 //  Created by Stefano Pozzi on 28/05/24.
@@ -22,7 +22,7 @@ enum HealthMetricContext: CaseIterable, Identifiable {
     }
 }
 
-struct ContentView: View {
+struct DashboardView: View {
     @State private var selectedStat: HealthMetricContext = .steps
     var isSteps: Bool { selectedStat == .steps }
     
@@ -89,7 +89,7 @@ struct ContentView: View {
             }
             .navigationTitle("Dashboard")
             .navigationDestination(for: HealthMetricContext.self) { metric in
-                Text(metric.title)
+                HealthDataListView(metric: metric)
             }
         }
         .tint(isSteps ? .pink : .indigo)
@@ -97,5 +97,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    DashboardView()
 }
